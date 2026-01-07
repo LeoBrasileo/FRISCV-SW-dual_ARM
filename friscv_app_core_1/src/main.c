8b4bd13d7265073efd0d64cb01e0d70908cd24ba
Leonel Braginski <leobraginski@gmail.com>
2026-01-07 12:25:57 +0100

    memory loop complete

--- friscv_app_core_1/src/main.c
+++ friscv_app_core_1/src/main.c
@@ -16,9 +16,6 @@ int main(void)
 	//Disable cache on OCM
 	Xil_SetTlbAttributes(0x0DB91000,0x14de2); // S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0
 
-	u32 delay_cpu_us = clk_compute_delay_us(CLK_CPU_FREQ_KHZ);
-	u32 delay_mem_us = clk_compute_delay_us(CLK_MEM_FREQ_KHZ);
-
 	u32 addr;
 	u32 word;
 	u32 data;
@@ -26,26 +23,21 @@ int main(void)
 	u32 size;
 
 	while (1) {
-//		cpu_clk_up();
-//		delay_usec(delay_cpu_us);
-//		cpu_clk_down();
-//		delay_usec(delay_cpu_us);
-
 		// Instruction Memory Phase
 		if (i_mem_enable_out()) {
 
+			// addr is byte address from CPU
+			// so we need to shift it to a word-addressed (u32 pointer)
 			addr = receive_i_mem_address();
 			word = addr >> 2;
 
-			/* Make sure we see latest DRAM */
+			// Invalidate Cache
 			Xil_DCacheInvalidateRange((UINTPTR)&COMM_MEM[word],sizeof(u32));
 
 			data = COMM_MEM[word];
 
-			/* Drive instruction back to CPU */
+			// Drive instruction back to CPU
 			send_i_mem_data(data);
-
-			cpu_clk_half_cycle();
 
 		// Data Memory Phase
 		} else if (d_mem_enable_out()) {
@@ -56,26 +48,31 @@ int main(void)
 			size = d_size_out();
 
 			if (!d_write_out())	{
-				// READ
+				// READ -->
 
 				Xil_DCacheInvalidateRange((UINTPTR)&COMM_MEM[word], sizeof(u32));
 
 				data = COMM_MEM[word];
 				send_d_mem_data(data);
+
+				//LOG("[ARM-1] Read memory\n");
+
 			} else {
-				// WRITE
+				// WRITE -->
 
 				data = receive_d_mem_data();
 
 				switch(size){
-					case 0: { //Byte
+					case 0: {
+						//Byte
 						u32 tmp = COMM_MEM[word];
 						tmp &= ~(0xFF << (offset * 8));
 						tmp |=  ((data & 0xFF) << (offset * 8));
 						COMM_MEM[word] = tmp;
 						break;
 					}
-					case 1: { //Half Word
+					case 1: {
+						//Half Word
 						u32 tmp = COMM_MEM[word];
 						if ((offset & 0x2) == 0) {
 							tmp &= ~0x0000FFFF;
@@ -87,7 +84,8 @@ int main(void)
 						COMM_MEM[word] = tmp;
 						break;
 					}
-					case 2: { //Word
+					case 2: {
+						//Word
 						COMM_MEM[word] = data;
 						break;
 					}
@@ -96,10 +94,12 @@ int main(void)
 				}
 
 				Xil_DCacheFlushRange((UINTPTR)&COMM_MEM[word], sizeof(u32));
-			}
 
-			cpu_clk_half_cycle();
+				//LOG("[ARM-1] Write memory\n");
+			}
 		}
+
+		cpu_clk_half_cycle();
 
 	}
 
